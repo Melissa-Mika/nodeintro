60358ea45eda3dc56f88413e75d726e83ecf7f99
MIKA <mm0970610@dt038523.otc.edu>
2023-04-26 12:53:28 -0500

    added axios

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import chalk from 'chalk';
+import axios from 'axios';
 
 console.log(chalk.blue('Hello world!'));
+import scripts from './scripts.js';
+import {configs} from '.scripts.js';
+
+scripts();
+configs();
+
+axios.get(import axios from 'axios')
