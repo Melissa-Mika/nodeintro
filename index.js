423ae6a1a334eb0664ed87add5b2607a32147ed8
MIKA <mm0970610@dt038523.otc.edu>
2023-04-26 13:06:48 -0500

    added env

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ import {configs} from '.scripts.js';
 scripts();
 configs();
 
-axios.get(import axios from 'axios')
+import {config} from 'dotenv';
+import {console} from 'console';
+config()
+console.log(process.env.API_KEY);
+
+let api_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
+axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=cd68dac4dd30c317aa425410192f2177&language=en-US&page=1")
+.then (res.data)
+
